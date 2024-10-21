ba9b17c1522774ffe4973a1b169e2baba9ad06d3
lizethlerma01 <llerma419@gmail.com>
2024-10-21 10:22:33 -0600

    Delete SegundaUnidad_LHKL directory

--- SegundaUnidad_LHKL/P19_Array1.php
+++
@@ -1,7 +0,0 @@
-<?php
-$array=[
-"Valor1"=> "Pedro",
-"Valor2"=> "Hobi",
-];
-var_dump($array);
-?>
--- SegundaUnidad_LHKL/P24_Array6.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*CBTIS 89
- Programa que 
- Lerma Hernández Karla Lizeth
- 3°A Programación T.M
- */
