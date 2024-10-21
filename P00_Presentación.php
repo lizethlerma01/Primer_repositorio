78fd79c02eb4d178b97027e014deb4468c203664
lizethlerma01 <llerma419@gmail.com>
2024-10-21 09:31:14 -0600

    Add files via upload

---
+++ P00_Presentación.php
@@ -0,0 +1,16 @@
+<?php
+/*P00_Presentacion
+CBTIS 89
+ Programa que almacena datos en un arreglo y posteriormente los imprime
+ Lerma Hernández Karla Lizeth
+ 3°A Programación T.M
+ */
+
+ echo "Mi nombre es:Karla Lizeth Lerma Hernández";
+ echo "<br>";
+ echo "Estudio en el CBTIS 89";
+ echo "<br>";
+ echo "Estoy estudiando la carrera de programación 3°A Matutino";
+ echo "<br>";
+ echo "Vivo en Belisario Domínguez, Barrio de Analco";
+?>
