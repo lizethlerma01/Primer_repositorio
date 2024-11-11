c040b9bcfe60b386b88bb4b053c359113c1a5a42
lizethlerma01 <llerma419@gmail.com>
2024-11-11 08:36:33 -0600

    Add files via upload

--- Unidad3_LermaHernandez/P37_Array17.php
+++ Unidad3_LermaHernandez/P37_Array17.php
@@ -1,10 +1,10 @@
-<?php 
-/* CBTIS89
-Programa que dise los numeros pares
-
-3°A Matutino
-*/
-
+<?php
+/*CBTIS 89
+ Programa que almacena números positivos y negativos en un arreglo y posteriormente los imprime
+ Lerma Hernández Karla Lizeth
+ 3°A Programación T.M
+ */
+ 
 $arraynumeros = array(-1,-5,7,-9,5,8,-24,56,-46,90,-65,33,44,-23,-10);
 $arraypositivos = array();
 $arraynegativos = array();
